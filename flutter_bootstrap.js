0b9ee91b8af7fa8b5e5b0dcd6f3d450365837735
wahyuunt97 <wahyuunt97@users.noreply.github.com>
2026-01-17 04:17:11 +0000

    deploy: wahyuunt97/wahyu_portfolio@7c412533168139c5797bd3150e22633ddeef8382

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3669290792"
+    serviceWorkerVersion: "2919228820"
   }
 });
